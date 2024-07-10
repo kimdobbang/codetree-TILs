cd0b07bd4fd4eb020904b4fbafe50649b559a457
kim doi <thisisdoemail@gmail.com>
2024-07-10 23:05:48 +0900

    Title: 달에서 무게 구하기, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240710/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int w = 13;
+        double rate = 0.165;
+        double result = w * rate;
+        System.out.printf("%d * %.6f = %.6f", w, rate, result);
+    }
+}
