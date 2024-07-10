fe029ac1f36726fe7e57be72386c59a38b7914bb
kim doi <thisisdoemail@gmail.com>
2024-07-10 22:54:54 +0900

    Title: 세 정수형 변수 선언, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240710/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        StringBuilder sb = new StringBuilder();
+        int a = 7;
+        int b = 23;
+        int c = 30;
+        System.out.println(a + " + " + b + " = " + c );
+        
+    }
+}
