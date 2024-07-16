3268664463cda9f61225bb2255420f6146c27c7f
kim doi <thisisdoemail@gmail.com>
2024-07-16 09:10:28 +0900

    Title: 데이터 교환, Time: 82ms, Memory: 8MB, Status: Passed - CodeTree

--- 240716/데이터 교환/exchange-data.java
+++ 240716/데이터 교환/exchange-data.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
+        // 변수 선언
         int a = 5, b = 6, c = 7;
-        int temp;
-        int temp2;
 
-        temp = b;
-        temp2 = c;
-        b = a;
-        c = temp;
-        a = temp2;
+        // 교체
+        int temp = a;
+        a = c;
+        c = b;
+        b = temp;
 
+        // 출력
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
