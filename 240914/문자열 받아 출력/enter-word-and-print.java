1e076b0d7ed62eda866f30ef1e0dea4506a46412
kim doi <thisisdoemail@gmail.com>
2024-09-14 16:41:45 +0900

    Title: 문자열 받아 출력, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240914/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,11 @@
+// import java.util.Scanner;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Scanner sc = new Scanner(System.in);
+        // String s = sc.next();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(s);
+    }
+}
