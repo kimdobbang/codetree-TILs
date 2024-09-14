35970be55f029aad36d08268355d003c9004b5fc
kim doi <thisisdoemail@gmail.com>
2024-09-14 16:18:14 +0900

    Title: 세 실수의 반올림, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240914/세 실수의 반올림/rounding-of-three-actual-numbers.java
+++ 240914/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -1,14 +1,14 @@
-import java.io.*;
+import java.io.StringBuilder;
+import java.io.BufferedReader;
+
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < 3; i++) {
             double num = Double.parseDouble(br.readLine());
             sb.append(String.format("%.3f", num)).append("\n");
         }
-
         System.out.print(sb);
     }
 }
