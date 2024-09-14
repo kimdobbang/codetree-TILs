af7f60ca59ded14548caa615d2add0619bada7cb
kim doi <thisisdoemail@gmail.com>
2024-09-14 16:34:18 +0900

    Title: 문자 받아 출력, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

--- 240914/문자 받아 출력/enter-char-and-print.java
+++ 240914/문자 받아 출력/enter-char-and-print.java
@@ -1,11 +1,10 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // String s = sc.next();// 문자열 입력
-        // char c = s.charAt(0);// 입력받은 문자열의 첫 번째 문자 추출
-        char c = sc.next().charAt(0);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char c = br.readLine().charAt(0);
 
         System.out.print(c);
     }
