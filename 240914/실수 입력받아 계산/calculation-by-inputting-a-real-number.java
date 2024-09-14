ac890d62e7407b706e8325d148b6524e5548d081
kim doi <thisisdoemail@gmail.com>
2024-09-14 16:01:46 +0900

    Title: 실수 입력받아 계산, Time: 104ms, Memory: 9MB, Status: Passed - CodeTree

--- 240914/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
+++ 240914/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -3,11 +3,12 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         double a = Double.parseDouble(br.readLine());
         double b = Double.parseDouble(br.readLine());
         // double result = Math.round((a + b)*100.0)/100.0;
         // System.out.println(result);
-        double result = a + b;
-        System.out.println(String.format("%.2f", result));
+        sb.append(String.format("%.2f", a + b));
+        System.out.println(sb);
     }
 }
