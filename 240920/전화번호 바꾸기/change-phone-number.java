47cb65d11af7ddd494d568d8cfe1f949026df23c
kim doi <thisisdoemail@gmail.com>
2024-09-20 20:52:20 +0900

    Title: 전화번호 바꾸기, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

--- 240920/전화번호 바꾸기/change-phone-number.java
+++ 240920/전화번호 바꾸기/change-phone-number.java
@@ -5,12 +5,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(),"-");
+        StringBuilder sb = new StringBuilder();
         
         String pre = st.nextToken();
         String x = st.nextToken();
         String y = st.nextToken();
 
-        System.out.println(pre + "-" + y +"-" + x);
+        sb.append(pre).append("-").append(y).append("-").append(x);
+
+        System.out.println(sb);
 
     }
 }
