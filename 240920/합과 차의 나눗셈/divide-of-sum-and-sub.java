266ffab71186ada017dd2141daad008ab181611c
kim doi <thisisdoemail@gmail.com>
2024-09-20 21:56:15 +0900

    Title: 합과 차의 나눗셈, Time: 180ms, Memory: 9MB, Status: Passed - CodeTree

--- 240920/합과 차의 나눗셈/divide-of-sum-and-sub.java
+++ 240920/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st =new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
