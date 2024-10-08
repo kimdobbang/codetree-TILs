10d707b6d693d16f5c7e42cc0c3579d9218a7147
kim doi <thisisdoemail@gmail.com>
2024-10-09 01:06:14 +0900

    Title: 세 수의 합과 평균 구하기, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

--- 241009/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.java
+++ 241009/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.java
@@ -7,9 +7,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st =new StringTokenizer(br.readLine());
 
-        int a = Integer.ParseInt(st.nextToken());
-        int b = Integer.ParseInt(st.nextToken());
-        int c = Integer.ParseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
         sb.append(a + b + c).append("\n").append((a + b + c)/3);
         System.out.print(sb);
