3fcd85198b17c5930af1c62b0010731765f370b5
kim doi <thisisdoemail@gmail.com>
2024-12-01 13:59:56 +0900

    Title: 큰 수에서 빼기, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241201/큰 수에서 빼기/subtract-from-large-number.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a > b) {
+            System.out.println(a - b);
+        } else {
+            System.out.println(b - a);
+        }
+    }
+}
