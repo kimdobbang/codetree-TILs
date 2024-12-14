6d4ed7b83b6d10187cec359458e4f93f513841f6
kim doi <thisisdoemail@gmail.com>
2024-12-15 03:39:54 +0900

    Title: 살 수 있는 물건 2, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241215/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*; 
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        if (n >= 3000) {
+            sb.append("book");
+        } else if (n >= 1000) {
+            sb.append("mask");
+        } else if (n >= 500) {
+            sb.append("pen");
+        } else {
+            sb.append("no");        
+        }
+        
+        System.out.println(sb);
+    }
+}
