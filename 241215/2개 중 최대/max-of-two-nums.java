8a148abc9ff2203cf5a364de5744207f4f79ff30
kim doi <thisisdoemail@gmail.com>
2024-12-15 03:31:59 +0900

    Title: 2개 중 최대, Time: 84ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241215/2개 중 최대/max-of-two-nums.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*; 
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int maxVal = (a > b) ? a : b;
+        System.out.println(maxVal);
+    }
+}
