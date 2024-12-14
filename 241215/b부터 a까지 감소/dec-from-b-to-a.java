0939d1b3c1fb5273f7487c9bf85e26b32ff12d27
kim doi <thisisdoemail@gmail.com>
2024-12-15 03:16:37 +0900

    Title: b부터 a까지 감소, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241215/b부터 a까지 감소/dec-from-b-to-a.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        for(;b >= a; b--) {
+            sb.append(b).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
