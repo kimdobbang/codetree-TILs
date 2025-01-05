76ac88564200b252c70f0a89f2b2127e135faf91
kim doi <thisisdoemail@gmail.com>
2025-01-06 01:08:51 +0900

    Title: 수의 계절은, Time: 83ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250106/수의 계절은/season-of-num.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+
+        if (m < 3 ) {
+            System.out.print("Winter");
+        } else if ( m < 6) {
+            System.out.print("Spring");
+        } else if ( m < 9) {
+            System.out.print("Summer");
+        } else if ( m < 12) {
+            System.out.print("Winter");
+        } else {
+            System.out.print("Winter");
+        }
+    }
+}
