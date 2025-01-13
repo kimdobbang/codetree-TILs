d23c1d87e1824f3c1269f335b18dd4fd09b0c4a9
kim doi <thisisdoemail@gmail.com>
2025-01-13 19:46:22 +0900

    Title: 최댓값 구하기, Time: 85ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250113/최댓값 구하기/maximum-value.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int max = -100;        
+        
+        if (a > max) {
+            max = a;
+        }
+        if ( b > max) {
+            max = b;
+        }
+        if ( c > max) {
+            max = c;
+        }
+
+        System.out.print(max);
+    }
+}
