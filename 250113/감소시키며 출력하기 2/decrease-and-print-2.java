a39b5e709ff4f8f524543e463fab617767e12471
kim doi <thisisdoemail@gmail.com>
2025-01-13 23:28:54 +0900

    Title: 감소시키며 출력하기 2, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250113/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int i = 26;
+        while(i >= 10) {
+            System.out.print(i + " ");
+            i--;
+        }
+    }
+}
