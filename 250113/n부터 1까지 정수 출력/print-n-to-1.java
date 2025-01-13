b6b67563ce855fe1ad4027ce0a326227d01a6362
kim doi <thisisdoemail@gmail.com>
2025-01-13 23:31:08 +0900

    Title: n부터 1까지 정수 출력, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250113/n부터 1까지 정수 출력/print-n-to-1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        while (1 <= n) {
+            System.out.print(n + " ");
+            n--;
+        }
+    }
+}
