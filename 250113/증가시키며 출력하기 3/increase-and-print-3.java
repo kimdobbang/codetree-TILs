bebd00ee804f74434ab5383e290514f0a46c9221
kim doi <thisisdoemail@gmail.com>
2025-01-13 23:13:14 +0900

    Title: 증가시키며 출력하기 3, Time: 80ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250113/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int i = 10;
+        while(i <= 26) {
+            System.out.print(i + " ");
+            i += 2;
+        }
+    }
+}
