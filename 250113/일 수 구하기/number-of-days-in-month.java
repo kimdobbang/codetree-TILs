96c8359996517a3fac5a8ca88142ad442cb69e76
kim doi <thisisdoemail@gmail.com>
2025-01-13 19:36:44 +0900

    Title: 일 수 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250113/일 수 구하기/number-of-days-in-month.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // 28: 2
+        // 30: 6, 9, 11
+        // 31: 1, 3, 5, 7, 8, 10, 12
+        if (n == 2){
+            System.out.print(28);
+        }if else (n == 6 || n == 9 || n == 11) {
+            System.out.print(30);
+        } else {
+            System.out.print(31);
+        }
+    }
+}
