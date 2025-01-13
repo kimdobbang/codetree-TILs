43f00bd52b737e9ae04b3403a1b68f8f13181603
kim doi <thisisdoemail@gmail.com>
2025-01-13 20:20:19 +0900

    Title: 중앙값 구하기, Time: 247ms, Memory: 14MB, Status: Passed - CodeTree

--- 250113/중앙값 구하기/find-the-median.java
+++ 250113/중앙값 구하기/find-the-median.java
@@ -5,16 +5,32 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
+        // int a = Integer.parseInt(st.nextToken());
+        // int b = Integer.parseInt(st.nextToken());
+        // int c = Integer.parseInt(st.nextToken());
 
-        if (a > b && a < c) {
-            System.out.print(a);
-        } else if (b > a && b < c) {
-            System.out.print(b);
-        } else if (c > a && c < b) {
-            System.out.print(c);
+        // compare
+        Integer[] arr = new Integer[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(arr, (o1, o2) -> o1 - o2);
+
+        System.out.print(arr[1]);
+
+        // 배열 정렬
+        // int[] arr = {a, b, c};
+        // Arrays.sort(arr);
+        // System.out.print(arr[1]);
+
+        // 조건식
+        // if ((a > b && a < c) || (a > c && a < b)) {
+        //     System.out.print(a);
+        // } else if ((b > a && b < c) || (b > c && b < a)) {
+        //     System.out.print(b);
+        // } else if ((c > a && c < b) || (c > b && c < a)) {
+        //     System.out.print(c);
+        // }
     }
 }
