af2f4402f547e6e49874379ab351d9cea196c8da
kim doi <thisisdoemail@gmail.com>
2025-01-27 16:49:22 +0900

    Title: 369 게임, Time: 101ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250127/369 게임/369-game.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            if (isZero(i)) {
+                sb.append(0).append(" ");
+            } else {
+                sb.append(i).append(" ");
+            }
+        }
+
+        System.out.print(sb.toString().trim());
+    }
+
+    public static boolean isZero(int num){
+
+        if (num % 3 == 0 || is369(num)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean is369(int num) {
+        String nums = Integer.toString(num);
+        for(char n : nums.toCharArray()) {
+            if (n == '3' | n == '6' || n == '9') {
+                return true;
+            }
+        }
+        return false;
+    }
+}
