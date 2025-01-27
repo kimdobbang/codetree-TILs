dea73d8ef97fece4549a3698115f0dcb7529f052
kim doi <thisisdoemail@gmail.com>
2025-01-27 17:26:13 +0900

    Title: 369 게임, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

--- 250127/369 게임/369-game.java
+++ 250127/369 게임/369-game.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // Please write your code here.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
@@ -22,22 +21,29 @@ public class Main {
         System.out.print(sb);
     }
 
-    public static boolean isZero(int num){
+    // 숫자가 3의 배수이거나 3, 6, 9 포함여부 확인
+    public static boolean isZero(int num) {
+    if (num % 3 == 0) { // 3의 배수면 바로 true 반환
+        return true;
+    }
 
-        if (num % 3 == 0 || is369(num)) {
+    // 숫자로 처리
+    while (num > 0) {
+        int digit = num % 10; // 마지막 자릿수 확인
+        if (digit == 3 || digit == 6 || digit == 9) {
             return true;
         }
-
-        return false;
+        num /= 10; // 다음 아래자릿수 확인
     }
 
-    public static boolean is369(int num) {
-        String nums = Integer.toString(num);
-        for(char n : nums.toCharArray()) {
-            if (n == '3' || n == '6' || n == '9') {
-                return true;
-            }
-        }
-        return false;
-    }
+    // 문자로 처리
+    // String nums = Integer.toString(num); // 각 자리 확인
+    // for (char n : nums.toCharArray()) {
+        // if (n == '3' || n == '6' || n == '9') {
+            // return true;
+        // }
+    // }
+    return false;
+}
+
 }
