18b8506e7db4d3a188b224b874382aff1277c541
kim doi <thisisdoemail@gmail.com>
2025-01-30 00:21:41 +0900

    Title: 배수의 개수 2, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250130/배수의 개수 2/number-of-multipliers-2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
+
+        int n;
+        for (int i = 0; i < 10; i++) {
+            n = Integer.parseInt(br.readLine());
+            if (n % 2 != 0) {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
