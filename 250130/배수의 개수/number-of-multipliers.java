76ba3e40048ebfafa4e137d9a6f9292912000a2e
kim doi <thisisdoemail@gmail.com>
2025-01-30 00:25:20 +0900

    Title: 배수의 개수, Time: 85ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250130/배수의 개수/number-of-multipliers.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cntT = 0;
+        int cntF = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if (n % 3 == 0) {
+                cntT++;
+            }
+            if (n % 5 == 0) {
+                cntF++;
+            }
+        }
+        System.out.print(cntT + " " + cntF);
+    }
+}
