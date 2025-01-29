2b46e0e78fafe87fd7f3dd35639df39580a911c7
kim doi <thisisdoemail@gmail.com>
2025-01-30 00:57:43 +0900

    Title: 윤년 횟수 구하기, Time: 83ms, Memory: 9MB, Status: Passed - CodeTree

--- 250130/윤년 횟수 구하기/number-of-leap-years.java
+++ 250130/윤년 횟수 구하기/number-of-leap-years.java
@@ -5,18 +5,20 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int cnt = 0;
+        // int cnt = 0;
                 
         int n = Integer.parseInt(br.readLine());
+        // O(1) 방식으로 윤년 개수 계산
+        int cnt = (n / 4) - (n / 100) + (n / 400);
 
-        for (int i = 1; i <= n; i++) {
-            if (i % 4 == 0) {
-                cnt++;
-            } 
-            if (i % 100 == 0 && i % 400 != 0) {
-                cnt--;
-            }
-        }
+        // for (int i = 1; i <= n; i++) {
+            // if (i % 4 == 0) {
+                // cnt++;
+            // } 
+            // if (i % 100 == 0 && i % 400 != 0) {
+                // cnt--;
+            // }
+        // }
         System.out.print(cnt);
     }
 }
