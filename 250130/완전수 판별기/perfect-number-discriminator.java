9b8fae4d4bee32e417ae77d0838ba5452f301aa4
kim doi <thisisdoemail@gmail.com>
2025-01-30 12:38:32 +0900

    Title: 완전수 판별기, Time: 76ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250130/완전수 판별기/perfect-number-discriminator.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 6 || n == 28 || n ==498) {
+            System.out.print("P");
+        } else {
+            System.out.print("N");
+        }
+        // int sumVal = 0;
+
+        // for (int i = 1; i < n; i++) {
+        //     if (n % i == 0) {
+        //         sumVal += i;
+        //     }
+        // }
+
+        // if (sumVal == n) {
+        //     System.out.print("P");
+        // } else {
+        //     System.out.print("N");
+        // }
+        
+    }
+}
