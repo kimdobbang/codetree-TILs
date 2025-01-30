984389cca5c969f2e33225a6e7088c79964b85af
kim doi <thisisdoemail@gmail.com>
2025-01-30 22:41:30 +0900

    Title: 다양하게 출력, Time: 84ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250130/다양하게 출력/print-in-variety.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        System.out.println("Total days in Year\n365\nCircumference rate\n3.1415926535");
+    }
+}
