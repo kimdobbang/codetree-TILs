d298359bf90f54ac201657379a34dc8d9d2c3989
kim doi <thisisdoemail@gmail.com>
2025-01-30 17:32:05 +0900

    Title: a의 배수들의 곱, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

--- 250130/a의 배수들의 곱/product-of-multiples-of-a.java
+++ 250130/a의 배수들의 곱/product-of-multiples-of-a.java
@@ -10,12 +10,13 @@ public class Main {
         int b = Integer.parseInt(st.nextToken());
         int prob = 1;
 
-        // b이하의 a 배수만 순회하면 불필요한 반복 x
+        // b이하의 a 배수만 순회하면 불필요한 반복 x, 조건검사 x O(b/a) 
+        // if (i % a == 0) 방식도 괜찮지만, i += a로 하면 더 최적화
         for (int i = a; i <= b; i += a ) {
             prob *= i;
         }
 
-        // 모든 b를 순회
+        // 모든 b를 순회 O(b)
         // for (int i = 1; i <= b; i++) {
         //     if (i % a == 0) {
         //         prob *= i;
