b0bd44101e2a6803ae4f641203a2f88d1d94f21b
kim doi <thisisdoemail@gmail.com>
2025-01-30 17:26:36 +0900

    Title: a의 배수들의 곱, Time: 81ms, Memory: 9MB, Status: Passed - CodeTree

--- 250130/a의 배수들의 곱/product-of-multiples-of-a.java
+++ 250130/a의 배수들의 곱/product-of-multiples-of-a.java
@@ -8,29 +8,29 @@ public class Main {
 
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        // int prob = 1;
+        int prob = 1;
 
-        // for(int i = 1; i <= b; i++) {
-        //     if (i % a == 0) {
-        //         prob *= i;
-        //     }
-        // }
-        // System.out.print(prob);
+        for(int i = 1; i <= b; i++) {
+            if (i % a == 0) {
+                prob *= i;
+            }
+        }
+        System.out.print(prob);
 
-        //  재귀(비효율적)
-        System.out.print(sol(a, b));
+        //  재귀
+        // System.out.print(sol(a, b));
         
     }
 
-    public static int sol(int a, int b) {
-        if (b == 0) {
-            return 1;
-        } else {
-            if (b % a == 0) {
-                return b * sol(a, b - 1);
-            } else {
-                return sol(a, b - 1);
-            }
-        }
-    }
+    // public static int sol(int a, int b) {
+    //     if (b == 0) {
+    //         return 1;
+    //     } else {
+    //         if (b % a == 0) {
+    //             return b * sol(a, b - 1);
+    //         } else {
+    //             return sol(a, b - 1);
+    //         }
+    //     }
+    // }
 }
