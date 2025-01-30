90224b7625a043ebc60b3c99c1e7a96a3e835d6e
kim doi <thisisdoemail@gmail.com>
2025-01-30 12:49:06 +0900

    Title: 입력받은 수의 합과 평균, Time: 89ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250130/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+                
+        int n = Integer.parseInt(br.readLine().trim());
+        int sumVal = 0;
+        double avgVal;
+
+        for (int i = 0; i < n; i++) {
+            sumVal += Integer.parseInt(br.readLine().trim());
+        }
+
+        avgVal = (double) sumVal / n;
+
+        System.out.print(sumVal + " " + avgVal);
+        
+    }
+}
