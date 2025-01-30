0edc2ed7a55d8ec06ac7ee1acf20143478f56cf4
kim doi <thisisdoemail@gmail.com>
2025-01-30 12:51:12 +0900

    Title: 입력받은 수의 합과 평균, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

--- 250130/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.java
+++ 250130/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.java
@@ -16,7 +16,7 @@ public class Main {
 
         avgVal = (double) sumVal / n;
 
-        System.out.printf("%d %1.f", sumVal, avgVal);
+        System.out.printf("%d %.1f", sumVal, avgVal);
         
     }
 }
