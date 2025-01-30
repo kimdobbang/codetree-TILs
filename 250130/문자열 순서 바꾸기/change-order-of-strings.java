c2508a7b0c122366f2487fcfdd01987e9d3038d5
kim doi <thisisdoemail@gmail.com>
2025-01-30 23:10:32 +0900

    Title: 문자열 순서 바꾸기, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250130/문자열 순서 바꾸기/change-order-of-strings.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+        System.out.println(b);
+        System.out.println(a);
+        
+    }
+}
