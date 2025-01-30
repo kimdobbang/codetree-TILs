c565deeb67bb1e9035bb190a04d7602cd5f765de
kim doi <thisisdoemail@gmail.com>
2025-01-30 12:18:41 +0900

    Title: 5의 배수의 합, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

--- 250130/5의 배수의 합/sum-of-specific-condition-multiple.java
+++ 250130/5의 배수의 합/sum-of-specific-condition-multiple.java
@@ -1,11 +1,9 @@
 import java.io.*;
 import java.util.*;
-import java.math.*;
 
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int a = Integer.parseInt(st.nextToken());
