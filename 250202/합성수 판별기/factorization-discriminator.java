1348c6fde8e6b574037891f8e7d3c835a3e12367
kim doi <thisisdoemail@gmail.com>
2025-02-02 23:45:57 +0900

    Title: 합성수 판별기, Time: 92ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 250202/합성수 판별기/factorization-discriminator.java
+++ 250202/합성수 판별기/factorization-discriminator.java
@@ -8,21 +8,21 @@ public class Main {
                 
         int n = Integer.parseInt(br.readLine());
         
-        // boolean isC = false;
+        boolean isC = false;
         for (int i = 2; i < n; i++) {
             if (n % i == 0) {
-                // isC = true;
-                System.out.print("C");
-                return;
+                isC = true;
+                // System.out.print("C");
+                // return;
             }
         }
         System.out.print("N");
 
-        // if (isC) {
-            // System.out.print("C");
-        // } else {
-            // System.out.print("N");
-        // }
+        if (isC) {
+            System.out.print("C");
+        } else {
+            System.out.print("N");
+        }
                 
     }
 }
