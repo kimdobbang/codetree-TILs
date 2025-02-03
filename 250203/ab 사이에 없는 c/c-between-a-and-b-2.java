3416c268e914327b8524ca5f846868c7f599cd3c
kim doi <thisisdoemail@gmail.com>
2025-02-03 20:27:40 +0900

    Title: ab 사이에 없는 c, Time: 1196ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250203/ab 사이에 없는 c/c-between-a-and-b-2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        boolean hasC = true;
+        for (int i = a; i <= b; b++) {
+            if (i % 3 != 0) {
+                hasC = false;
+                break;
+            }
+        }
+        
+        if (hasC) {
+            System.out.print("NO");
+        } else {
+            System.out.print("YES");
+        }
+        
+    }
+}
