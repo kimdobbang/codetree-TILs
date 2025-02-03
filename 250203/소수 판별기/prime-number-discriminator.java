ea58b9b28dcb0c8a2dff427f875beda672330acc
kim doi <thisisdoemail@gmail.com>
2025-02-03 20:43:40 +0900

    Title: 소수 판별기, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

--- 250203/소수 판별기/prime-number-discriminator.java
+++ 250203/소수 판별기/prime-number-discriminator.java
@@ -8,7 +8,7 @@ public class Main {
 
         boolean isPrime = true;
         for (int i = 2; i < n; i++) {
-            if (n & i == 0) {
+            if (n % i == 0) {
                 isPrime = false;
                 break;
             }
