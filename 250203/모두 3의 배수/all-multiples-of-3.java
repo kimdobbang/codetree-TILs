bc35aeb2bb55aa98c00154f190fc40072377fc3e
kim doi <thisisdoemail@gmail.com>
2025-02-03 21:30:20 +0900

    Title: 모두 3의 배수, Time: 91ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250203/모두 3의 배수/all-multiples-of-3.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean isPerfect = true;
+        for(int i = 0; i < 5; i++) {
+            if (i % 3 != 0) {
+                isPerfect = false;
+            }
+        }
+
+        if (isPerfect) {
+            System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
+    }
+}
