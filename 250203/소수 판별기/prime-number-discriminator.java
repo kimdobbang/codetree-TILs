a68bbead0d054f61f82fbd41d85c0108f6c47b84
kim doi <thisisdoemail@gmail.com>
2025-02-03 20:42:36 +0900

    Title: 소수 판별기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250203/소수 판별기/prime-number-discriminator.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));                
+        int n = Integer.parseInt(br.readLine());
+
+        boolean isPrime = true;
+        for (int i = 2; i < n; i++) {
+            if (n & i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            System.out.print("P");
+        } else {
+            System.out.print("C");
+        }
+                
+    }
+}
