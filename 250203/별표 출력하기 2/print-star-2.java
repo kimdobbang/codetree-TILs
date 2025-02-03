98e5eee5b658b724dddbadfe0f125b3425ef94c0
kim doi <thisisdoemail@gmail.com>
2025-02-03 22:18:36 +0900

    Title: 별표 출력하기 2, Time: 84ms, Memory: 9MB, Status: Passed - CodeTree

--- 250203/별표 출력하기 2/print-star-2.java
+++ 250203/별표 출력하기 2/print-star-2.java
@@ -8,7 +8,7 @@ public class Main {
                 
         int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; ++) {
+        for (int i = 0; i < n; i++) {
             // for (int j = n - i; j > 0; j --) {
             for (int j = 0; j < n - i; j++) {
                 sb.append("*").append(" ");
