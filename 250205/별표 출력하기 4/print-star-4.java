cc7984788b3012f3425c56ee3a763861762e4c77
kim doi <thisisdoemail@gmail.com>
2025-02-05 22:58:11 +0900

    Title: 별표 출력하기 4, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250205/별표 출력하기 4/print-star-4.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+                
+        int n = Integer.parseInt(br.readLine());
+        int star = n;
+
+        for(int i = 0; i < 2 * n - 1; i++) {
+            for(int j = 0; j < star; j++) {
+                sb.append("*").append(" ");
+            }
+            sb.append("\n");
+            
+            if(i < n - 1) {
+                star --;
+            } else {
+                star ++;
+            }
+        }
+        System.out.print(sb);                
+    }
+}
