ff832dc0f3f654aec163d9f672ff22e4f9532a7f
kim doi <thisisdoemail@gmail.com>
2025-02-05 23:29:18 +0900

    Title: 건너뛰고 별표 출력하기, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

--- 250205/건너뛰고 별표 출력하기/skip-and-print-a-star.java
+++ 250205/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -8,20 +8,42 @@ public class Main {
                 
         int n = Integer.parseInt(br.readLine());
 
-        // 길이가 n 인 직각 삼각형
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j <=i; j++) {
-                sb.append("*");
-            }
-            sb.append("\n").append("\n");
-        }
+        // // 길이가 n 인 직각 삼각형
+        // for(int i = 0; i < n; i++) {
+        //     for(int j = 0; j <=i; j++) {
+        //         sb.append("*");
+        //     }
+        //     sb.append("\n").append("\n");
+        // }
 
-        // 길이가 n - 1인 직각 삼각형
-        for(int i = n - 1; i > 0; i--) {
-            for(int j = 0 ; j < i; j++){
+        // // 길이가 n - 1인 직각 삼각형
+        // for(int i = n - 1; i > 0; i--) {
+        //     for(int j = 0 ; j < i; j++){
+        //         sb.append("*");
+        //     }
+        //     sb.append("\n").append("\n");
+        // }
+
+        int star = 1;
+
+        for(int i = 0; i < 2 * n - 1; i++) {
+            for(int j = 0; j < star; j++) {
                 sb.append("*");
             }
-            sb.append("\n").append("\n");
+            sb.append("\n");
+
+            // 한 줄 건너뛰기
+            if (i == 2 * n -2) {
+                continue;
+            } else {
+                sb.append("\n");
+            }
+
+            if(i < n - 1) {
+                star ++;
+            } else {
+                star --;
+            }
         }
 
         System.out.print(sb);                
