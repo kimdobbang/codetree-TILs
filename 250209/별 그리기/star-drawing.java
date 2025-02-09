40ca66005fbe725748561239972823fe0e616008
kim doi <thisisdoemail@gmail.com>
2025-02-09 22:16:23 +0900

    Title: 별 그리기, Time: 91ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 250209/별 그리기/star-drawing.java
+++ 250209/별 그리기/star-drawing.java
@@ -11,7 +11,6 @@ public class Main {
         i : 2 * n - 2
         위쪽 * : 2 * i + 1
         대칭 : n - 1 번째 줄
-        아래쪽 * : 2 * (i + 2 * n - 2 - i - i) + 1( i대신 a + b  -i 대입, a가 i, b가 2 * n - 2 - i)
         */
 
         for (int i = 0; i < 2 * n - 1; i++) {
@@ -23,7 +22,7 @@ public class Main {
             }
 
             // 별
-            int star = i < n ? 2 * i + 1: (2 * n + 3) -  (2 * i);
+            int star = i < n ? 2 * i + 1: (2 * n + n) -  (2 * i);
             for(int j = 0; j < star; j++) {
                 sb.append("*");
             }
