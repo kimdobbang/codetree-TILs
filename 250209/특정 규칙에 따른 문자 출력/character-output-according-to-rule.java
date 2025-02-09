00c1dc2d192be50ef87d0bc9620dcca73c381c02
kim doi <thisisdoemail@gmail.com>
2025-02-09 23:24:52 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 85ms, Memory: 9MB, Status: Passed - CodeTree

--- 250209/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
+++ 250209/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -5,17 +5,17 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-                
         int n = Integer.parseInt(br.readLine());
 
+        // 2n - 1 줄
         for (int i = 0; i < 2 * n - 1; i++) {
             // 공백
             for (int j = 0; j < n - 1 - i; j++) {
                 sb.append("  ");
             }
             // @
-            int cnt = i < n - 1 ? i : n + 1 - i;
-            for (int j = 0; j <= cnt; j++) {
+            int cnt = i < n ? i + 1 : 2 * n - 1  - i;
+            for (int j = 0; j < cnt; j++) {
                 sb.append("@ ");
             }
             sb.append("\n");
@@ -24,3 +24,8 @@ public class Main {
         System.out.print(sb);
     }
 }
+
+// 1인경우
+// n = 2, i = 2 >>> 3
+// n = 3, i = 4 >>> 5
+// n = 4, i = 6 >>> 7
