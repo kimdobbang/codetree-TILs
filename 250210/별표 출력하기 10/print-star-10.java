c65af6c8a7e2dc915cc7ac6c0b78c6dfb0f6a10d
kim doi <thisisdoemail@gmail.com>
2025-02-10 13:38:22 +0900

    Title: 별표 출력하기 10, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250210/별표 출력하기 10/print-star-10.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        int starE = 1;
+        int starO = n;
+        for (int i = 0; i < 2 * n; i++) {
+            // i짝수일때
+            if (i % 2 == 0) {
+                for(int j = 0; j < starE ; j++) {
+                    sb.append("* ");
+                }
+                starE++;
+            // i홀수일때
+            } else {
+                for (int j = 0; j < starO; j++) {
+                    sb.append("* ");
+                }
+                starO--;
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
