872087f7c2b3c307dd749dc9450a66299991c913
kim doi <thisisdoemail@gmail.com>
2025-02-11 19:24:35 +0900

    Title: 9-1 정사각형, Time: 101ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250211/9-1 정사각형/9-1-square.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        int cnt = 9;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(cnt);
+                cnt--;
+                if (cnt < 1) {
+                    cnt = 9;
+                }
+            }
+                sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
