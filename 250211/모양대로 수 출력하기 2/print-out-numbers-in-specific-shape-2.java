c56cc8eb939aebb1c44520b3b6f038593e9c4592
kim doi <thisisdoemail@gmail.com>
2025-02-11 19:13:41 +0900

    Title: 모양대로 수 출력하기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250211/모양대로 수 출력하기 2/print-out-numbers-in-specific-shape-2.java
+++ 250211/모양대로 수 출력하기 2/print-out-numbers-in-specific-shape-2.java
@@ -8,6 +8,7 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         int cnt = 2;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (cnt > 8){
@@ -15,8 +16,10 @@ public class Main {
                 }
                 sb.append(cnt).append(" ");
                 cnt += 2;
-            }
-            sb.append("\n");
+                if (cnt > 8){
+                    cnt = 2;
+                    }
+                sb.append("\n");
         }
         System.out.print(sb);
     }
