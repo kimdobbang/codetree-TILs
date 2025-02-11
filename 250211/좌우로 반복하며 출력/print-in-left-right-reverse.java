2f79410a6eea765c0dbbfb6822991f200d34c5e7
kim doi <thisisdoemail@gmail.com>
2025-02-11 19:50:05 +0900

    Title: 좌우로 반복하며 출력, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250211/좌우로 반복하며 출력/print-in-left-right-reverse.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+                
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            // 홀수 행
+            if (i % 2 == 0) {
+                for (int j = 0; j < n; j++) {
+                    sb.append(j + 1);
+                }
+                sb.append("\n");
+                continue;
+            }
+            // 짝수 행
+            for (int j = 0; j < n; j++) {
+                sb.append(n - j);
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
