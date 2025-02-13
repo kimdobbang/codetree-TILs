b77f82f50a9895e36ec87d9845cc430e720cfe6c
kim doi <thisisdoemail@gmail.com>
2025-02-13 17:20:52 +0900

    Title: 행에 따라 다르게 출력, Time: 111ms, Memory: 9MB, Status: Passed - CodeTree

--- 250213/행에 따라 다르게 출력/output-defferently-each-row.java
+++ 250213/행에 따라 다르게 출력/output-defferently-each-row.java
@@ -5,21 +5,23 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        
+        // 필요 변수 선언 및 입력(짝수행이 될 때 2가 더해져야하므로 0으로 초기화하고 짝수행일 떼 +=2 처리)
         int n = Integer.parseInt(br.readLine());
-        int num = 0;
+        int cnt = 0;
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
 
                 // 홀수 행
                 if (i % 2 == 0) {
-                    num ++;
-                    sb.append(num).append(" ");
+                    cnt ++;
+                    sb.append(cnt).append(" ");
                     continue;
                 }
                 // 짝수 행
-                num += 2;
-                sb.append(num).append(" ");
+                cnt += 2;
+                sb.append(cnt).append(" ");
                 
             }
             sb.append("\n");
