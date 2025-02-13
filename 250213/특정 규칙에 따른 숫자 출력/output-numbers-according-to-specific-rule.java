78824a4214e3493f084bedca1726cf7d1fa3afcc
kim doi <thisisdoemail@gmail.com>
2025-02-13 19:11:31 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 80ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250213/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int num = 1;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i <= j) {
+                    if (num == 10) {
+                        num = 1;
+                    }
+                    sb.append(num++).append(" ");
+                    continue;
+                }
+                sb.append("  ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
