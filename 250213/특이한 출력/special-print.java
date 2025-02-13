339cccff418b2a9a047554a732fb0e3deb193bea
kim doi <thisisdoemail@gmail.com>
2025-02-13 19:56:51 +0900

    Title: 특이한 출력, Time: 85ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250213/특이한 출력/special-print.java
@@ -0,0 +1,21 @@
+ import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sb.append("(").append(i).append(", ").append(j).append(") ");
+                if ((i + j) % 4 == 0) {
+                    sb.append("\n");
+                }
+            }
+        }
+        System.out.print(sb);
+    }
+}
