f1e02c148d833aa60ef52c53b62dc8599bd13afe
kim doi <thisisdoemail@gmail.com>
2025-02-13 17:57:33 +0900

    Title: 숫자 삼각형, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250213/숫자 삼각형/number-triangle.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                sb.append(j + 1).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
