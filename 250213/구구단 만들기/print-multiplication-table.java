7c5c4f53b52f8848ecfbf60929a87b782d202229
kim doi <thisisdoemail@gmail.com>
2025-02-13 17:45:49 +0900

    Title: 구구단 만들기, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250213/구구단 만들기/print-multiplication-table.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+//44
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i < 10; i++) {
+            for (int j = b; j >= a; j -= 2) {
+                sb.append(j).append(" * ").append(i).append(" = ").append(i * j);
+                if ( j > a) {
+                    sb.append(" / ");
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
