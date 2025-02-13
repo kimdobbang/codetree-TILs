ec66e5979b58c351db4e8ae17b8164dd5b71cc85
kim doi <thisisdoemail@gmail.com>
2025-02-13 19:32:29 +0900

    Title: 규칙에 맞는 출력, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

--- 250213/규칙에 맞는 출력/output-that-matches-the-rule.java
+++ 250213/규칙에 맞는 출력/output-that-matches-the-rule.java
@@ -1,21 +1,30 @@
 import java.io.*;
 import java.util.*;
 
+// i 번째 줄의 첫 번째 숫자는 n - i 이고, n - i 부터 시작해여 n 까지의 수가 출력되야 하므로, i 번째 줄의 j 번째 칸에는 n - i + j 를 출력
+
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        
-        for (int i = n; i > 0; i--) {
-            for (int j = 1; j <= n; j++) {
-                if (i <= j) {
-                    sb.append(j).append(" ");
-                }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                sb.append(n - i + j).append(" ");
             }
             sb.append("\n");
         }
+
+        // for (int i = n; i > 0; i--) {
+        //     for (int j = 1; j <= n; j++) {
+        //         if (i <= j) {
+        //             sb.append(j).append(" ");
+        //         }
+        //     }
+        //     sb.append("\n");
+        // }
         System.out.print(sb);
     }
 }
