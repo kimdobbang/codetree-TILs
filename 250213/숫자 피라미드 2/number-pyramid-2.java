56337b2fd5a60b28ea75ff178e2456d0ed514a07
kim doi <thisisdoemail@gmail.com>
2025-02-13 18:31:33 +0900

    Title: 숫자 피라미드 2, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250213/숫자 피라미드 2/number-pyramid-2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 1;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                sb.append(cnt).append(" ");
+                cnt++;
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
